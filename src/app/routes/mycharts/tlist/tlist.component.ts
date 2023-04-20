c0e28d124fbbffbcec5baed510a32fa786eae817
qqchen <785019257@qq.com>
2023-04-20 20:35:15 +0800

    旭日图

--- src/app/routes/mycharts/tlist/tlist.component.ts
+++ src/app/routes/mycharts/tlist/tlist.component.ts
@@ -3,48 +3,9 @@ import { FormArray, FormControl, FormGroup, UntypedFormBuilder, UntypedFormContr
 import { STColumn, STComponent } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { ModalHelper, _HttpClient } from '@delon/theme';
+import {ChartEChartsOption} from "@delon/chart/chart-echarts";
 
-const options = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-            isLeaf: true
-          }
-        ]
-      },
-      {
-        value: 'ningbo',
-        label: 'Ningbo',
-        isLeaf: true
-      }
-    ]
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-            isLeaf: true
-          }
-        ]
-      }
-    ]
-  }
-];
+
 
 @Component({
   selector: 'app-mycharts-tlist',
@@ -80,62 +41,282 @@ const options = [
   ]
 })
 export class MychartsTlistComponent implements OnInit {
-  validateForm!: UntypedFormGroup;
 
-  nzOptions: any[] | null = null;
-  values: any[] | null = null;
+  option: ChartEChartsOption = {
+    title: {
+      text: '甲苯',
+      textStyle: { fontSize: 20, align: 'center' },
+      left: 'center',
+      bottom: 0
+    },
+    series: {
+      type: 'sunburst',
+      data: [
+        {
+          name: '1d',
+          itemStyle: { color: '#d46c6b' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.24,
+              itemStyle: { color: '#d46c6b' },
+              label: {},
+              labelLine: { show: true },
+              nodeClick: false
+            },
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.24,
+              itemStyle: { color: '#d46c6b' },
+              label: {},
+              labelLine: { show: true },
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.61,
+              itemStyle: { color: '#d46c6b' },
+              label: {},
+              labelLine: { show: true },
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.15,
+              itemStyle: { color: '#d46c6b' },
+              label: {},
+              labelLine: { show: true },
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '2d',
+          itemStyle: { color: '#e08654' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#e08654' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.48,
+              itemStyle: { color: '#e08654' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.21,
+              itemStyle: { color: '#e08654' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '3d',
+          itemStyle: { color: '#e4b468' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#e4b468' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.45,
+              itemStyle: { color: '#e4b468' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.26,
+              itemStyle: { color: '#e4b468' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '7d',
+          itemStyle: { color: '#a189e7' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#a189e7' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.54,
+              itemStyle: { color: '#a189e7' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.12,
+              itemStyle: { color: '#a189e7' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '10d',
+          itemStyle: { color: '#5989cb' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#5989cb' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.56,
+              itemStyle: { color: '#5989cb' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.08,
+              itemStyle: { color: '#5989cb' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '14d',
+          itemStyle: { color: '#46b4c3' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#46b4c3' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.54,
+              itemStyle: { color: '#46b4c3' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.12,
+              itemStyle: { color: '#46b4c3' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '21d',
+          itemStyle: { color: '#6fb38a' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: null,
+              itemStyle: { color: '#6fb38a' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.72,
+              itemStyle: { color: '#6fb38a' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.09,
+              itemStyle: { color: '#6fb38a' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        },
+        {
+          name: '28d',
+          itemStyle: { color: '#b7ca6e' },
+          children: [
+            {
+              name: '溶剂型木器涂料（含腻子）',
+              value: 0.21,
+              itemStyle: { color: '#b7ca6e' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.57,
+              itemStyle: { color: '#b7ca6e' },
+              label: {},
+              nodeClick: false
+            },
+            {
+              name: '人造板',
+              value: 0.07,
+              itemStyle: { color: '#b7ca6e' },
+              label: {},
+              nodeClick: false
+            }
+          ]
+        }
+      ],
+      radius: [0, '50%'],
+      emphasis: { focus: 'ancestor' },
+      levels: [
+        { itemStyle: { color: '#fff' } },    //由内而外第一级别样式
+        {
+          r0: '15%',
+          r: '35%',
+          itemStyle: { borderWidth: 1.5 },
+          label: { align: 'center', fontSize: 14, rotate: '0', silent: true,color:"#fff" }
+        },
+        {                         //由内而外第二级别样式
+          r0: '35%',
+          r: '50%',
+          label: {
+            fontSize: 12,
+            color: '#333',
+            position: 'outside',   //标签显示外围
+            distance: 40,    //标签和图距离
+            rotate: 0     //标签旋转角度
+          },
+
+          labelLine: {
+            show: true     //引导线
+          },
+
+          itemStyle: { borderWidth: 1.5 }    //扇形边框
+        }
+      ]
+    }
+  };
 
   constructor(private fb: UntypedFormBuilder, private http: _HttpClient) {}
 
   ngOnInit(): void {
-    this.nzOptions = options;
-    this.validateForm = this.fb.group({
-      city: this.fb.array([])
-    });
-    this.addField();
+
   }
 
-  get city() {
-    return this.validateForm.get('city') as FormArray;
-  }
 
-  addField(e?: MouseEvent): void {
-    if (e) {
-      e.preventDefault();
-    }
 
-    let cityGroup = this.fb.group({
-      cityName: ['', Validators.required]
-    });
-    this.city.push(cityGroup);
-    console.log(this.city.value);
-  }
-
-  removeField(i: number, e: MouseEvent): void {
-    e.preventDefault();
-    if (this.city.length > 1) {
-      this.city.removeAt(i);
-    }
-  }
-
-  submitForm(): void {
-    /*if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
-    } else {
-      Object.values(this.validateForm.controls).forEach(control => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        }
-      });
-    }*/
-    console.log(this.validateForm.value);
-    this.http.post('http://localhost:8082/index/index', this.validateForm.value).subscribe((res: any) => {
-      console.log(res);
-    });
-  }
-
-  onChanges(values: any): void {
-    console.log(values, this.values);
-  }
 }
